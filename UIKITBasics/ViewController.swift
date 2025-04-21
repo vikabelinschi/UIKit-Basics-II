35851369d71edcdcb7ee0ba10ffca98b1220524b
Victoria Belinschi <victoria.belinschi@tui.co.uk>
2025-04-21 18:25:04 +0300

    Initial Commit

---
+++ UIKITBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKITBasics
+//
+//  Created by Victoria Belinschi   on 21.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
